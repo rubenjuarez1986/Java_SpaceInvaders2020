5e83b5cf0fc90c2f0692babaaf0826c547c565e5
Ruben <Ruben@DESKTOP-2EIGM9G>
2020-02-13 19:20:40 +0100

    con sonidos¡¡¡¡¡¡¡

---
+++ src/codigo/Explosion.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codigo;
+
+import java.awt.Image;
+import javax.sound.sampled.Clip;
+
+/**
+ *
+ * @author Ruben
+ */
+public class Explosion {
+    Image imagen1 = null;
+    Image imagen2 = null;
+    
+    public int posX=0;
+    public int posY=0;
+    
+    public int tiempoDeVida = 50;
+    
+    Clip sonidoExplosion;
+    
+    
+}
